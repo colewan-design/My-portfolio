6e3fed0177532e3bce3ac0176b66660efd9abea1
christian <cts.colewan@gmail.com>
2023-05-04 10:12:59 +0800

    changed navigation bar | Modified text color to match each background image

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ $(document).ready(function(){
       });
   });
 });
+
+document.addEventListener('scroll', function() {
+    var sections = document.querySelectorAll('section');
+    sections.forEach(function(section) {
+      if (isInViewport(section)) {
+        section.classList.add('active');
+      }
+    });
+  });
+  
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY === 0) {
+      navbar.classList.remove('transparent');
+    } else {
+      navbar.classList.add('transparent');
+    }
+  });
+  
+  
+  function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+  }
+  
