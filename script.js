a82f44921ccbfeebd9036c08726ac4341b6ca177
Christian Colewan <cts.colewan@.com>
2024-07-02 16:49:01 +0800

    initial setup

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    var navToggle = document.querySelector('header nav ul');
-    document.querySelector('header .burger-menu').addEventListener('click', function() {
-        navToggle.classList.toggle('active');
-    });
-});
